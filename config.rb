5f2fd66fd5f45dc5d3b1451f5153e078d2403322
SHINYA <sny.0625hks@gmail.com>
2018-04-19 12:48:31 +0900

    url fixed update

--- config.rb
+++ config.rb
@@ -48,7 +48,7 @@ configure :build do
   activate :minify_css
   activate :minify_javascript
 
-  config[:root] = "/reponame"
+  config[:root] = ""
 
   # リポジトリ名を host に設定しておく
   # こうすることで stylesheet_link_tag などで展開されるパスが
